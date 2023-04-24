4af5b6cd818500b9e9816b917547b8f179d138e0
Nora Luna <86637985+per-sephone@users.noreply.github.com>
2023-04-24 11:36:14 -0700

    basic oscillator class

---
+++ src/oscillator.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+class Oscillator(ABC):
+    def __init__(self, frequency=440, sampleRate=48000, duration=1.0):
+        self._frequency = frequency
+        self._sampleRate = sampleRate
+        self._duration = duration
+        self._stepSize = 1.0 / sampleRate
+        self._time = np.arange(0, self._duration, self._stepSize)
+
+    def generateWave(self):
+        pass
+
+class SineOscillator(Oscillator):
+    def __init__(self, frequency=440, sampleRate=48000, duration=1.0):
+        super().__init__(frequency=frequency, sampleRate=sampleRate, duration=duration)
+    
+    def generateWave(self):
+        return self._amplitude * np.sin(2. * np.pi * self._frequency * self._time)
