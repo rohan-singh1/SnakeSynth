0255d9d60d16b876835b9e23ca5f77373f5bff20
Rohan Singh <vicky94rohan@gmail.com>
2023-04-24 03:47:31 -0700

    Two different tones for first two white keys implemented and integrated as PoC

--- src/form.py
+++ src/form.py
@@ -1,4 +1,6 @@
 import os
+import sine_tone
+
 from pathlib import Path
 from PySide6.QtWidgets import QWidget
 from PySide6.QtCore import QFile
@@ -8,7 +10,7 @@ from PySide6.QtUiTools import QUiLoader
 class MainWidget(QWidget):
     def __init__(self):
         super(MainWidget, self).__init__()
-        self.load_ui()
+        MainWidget.win = self.load_ui()
 
     def load_ui(self):
         loader = QUiLoader()
@@ -17,3 +19,26 @@ class MainWidget(QWidget):
         ui_file.open(QFile.ReadOnly)
         win = loader.load(ui_file, self)
         ui_file.close()
+
+        # Connecting UI signals to their respective slots
+        win.pushButton_2.pressed.connect(self.handle_button_pressed)
+        win.pushButton_2.released.connect(self.handle_button_released)
+        win.pushButton_3.pressed.connect(self.handle_button2_pressed)
+        win.pushButton_3.released.connect(self.handle_button_released)
+        win.volume_knob.valueChanged.connect(self.handle_volume_changed)
+
+        return win
+
+    def handle_volume_changed(value):
+        print("Current volume: ", MainWidget.win.volume_knob.value())
+
+    def handle_button_pressed(toggled):
+        sine_tone.play_sine(2)
+        
+    def handle_button_released(toggled):
+        sine_tone.stop_playing()
+
+    def handle_button2_pressed(toggled):
+        sine_tone.play_sine(2.2)
+
+        
