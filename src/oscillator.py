4608a98db025017a5c3e842bbbb7b6c7e327366c
Nora Luna <86637985+per-sephone@users.noreply.github.com>
2023-04-24 14:07:52 -0700

    updated for pitch coefficient

--- src/oscillator.py
+++ src/oscillator.py
@@ -1,21 +1,30 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 import numpy as np
+import sounddevice as sd
 
 class Oscillator(ABC):
-    def __init__(self, frequency=440, sampleRate=48000, amplitude=1.0, duration=1.0):
-        self._frequency = frequency
-        self._sampleRate = sampleRate
-        self._amplitude = amplitude
-        self._duration = duration
-        self._stepSize = 1.0 / sampleRate
+    def __init__(self, frequency=440.0, sampleRate=48000, amplitude=np.iinfo(np.int16).max//4, duration=1.0):
+        self._fundamentalFrequency = 440.0
+        self._frequency: float = frequency
+        self._sampleRate: int = sampleRate
+        self._amplitude: float = amplitude
+        self._duration: float = duration
+        self._stepSize: float = 1.0 / sampleRate
         self._time = np.arange(0, self._duration, self._stepSize)
+        self._pitchCoefficient = self._frequency / self._fundamentalFrequency
 
     def generateWave(self):
         pass
 
 class SineOscillator(Oscillator):
-    def __init__(self, frequency=440, sampleRate=48000, amplitude=1.0, duration=1.0):
+    def __init__(self, frequency=440, sampleRate=48000, amplitude=np.iinfo(np.int16).max//4, duration=1.0):
         super().__init__(frequency=frequency, sampleRate=sampleRate, amplitude=amplitude, duration=duration)
     
     def generateWave(self):
-        return self._amplitude * np.sin(2. * np.pi * self._frequency * self._time)
+        return self._amplitude * np.sin(self._pitchCoefficient * np.pi * self._frequency * self._time)
+
+if __name__ == "__main__":
+    sine = SineOscillator()
+    wave = sine.generateWave()
+    sd.play(wave, sine._sampleRate)
+    sd.wait()
--- tests/test_oscillator.py
+++ tests/test_oscillator.py
@@ -12,4 +12,4 @@ def test_sine_oscillator():
     oscillator = SineOscillator(frequency=freq, sampleRate=sampleRate, duration=duration)
     wave = oscillator.generateWave()
     assert len(wave) == sampleRate * duration
-    assert np.allclose(wave, np.sin(2. * np.pi * freq * oscillator._time))
+    assert np.allclose(wave, np.sin(oscillator._pitchCoefficient * np.pi * freq * oscillator._time) * oscillator._amplitude)
