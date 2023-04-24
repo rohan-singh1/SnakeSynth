8aad6a9dbb3267de5b746270dff62e1ff971229a
Nora Luna <86637985+per-sephone@users.noreply.github.com>
2023-04-24 12:04:28 -0700

    test_oscillator added

--- src/oscillator.py
+++ src/oscillator.py
@@ -2,9 +2,10 @@ from abc import ABC, abstractmethod
 import numpy as np
 
 class Oscillator(ABC):
-    def __init__(self, frequency=440, sampleRate=48000, duration=1.0):
+    def __init__(self, frequency=440, sampleRate=48000, amplitude=1.0, duration=1.0):
         self._frequency = frequency
         self._sampleRate = sampleRate
+        self._amplitude = amplitude
         self._duration = duration
         self._stepSize = 1.0 / sampleRate
         self._time = np.arange(0, self._duration, self._stepSize)
@@ -13,8 +14,8 @@ class Oscillator(ABC):
         pass
 
 class SineOscillator(Oscillator):
-    def __init__(self, frequency=440, sampleRate=48000, duration=1.0):
-        super().__init__(frequency=frequency, sampleRate=sampleRate, duration=duration)
+    def __init__(self, frequency=440, sampleRate=48000, amplitude=1.0, duration=1.0):
+        super().__init__(frequency=frequency, sampleRate=sampleRate, amplitude=amplitude, duration=duration)
     
     def generateWave(self):
         return self._amplitude * np.sin(2. * np.pi * self._frequency * self._time)
---
+++ tests/test_oscillator.py
@@ -0,0 +1,15 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.oscillator import SineOscillator
+import numpy as np
+
+def test_sine_oscillator():
+    freq = 440
+    sampleRate = 48000
+    duration = 1.0
+    oscillator = SineOscillator(frequency=freq, sampleRate=sampleRate, duration=duration)
+    wave = oscillator.generateWave()
+    assert len(wave) == sampleRate * duration
+    assert np.allclose(wave, np.sin(2. * np.pi * freq * oscillator._time))
