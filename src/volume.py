3e1bad6fd7c9a9b03f06588fbd5b8355416c5b37
Nora <nob@pdx.edu>
2023-05-09 11:38:44 -0700

    updated gain and tests

--- src/volume.py
+++ src/volume.py
@@ -2,10 +2,10 @@ class Volume():
     def __init__(self, setting=9, offset=0):
         self._setting = setting
         self._offset = offset
-        self._volume = self.gain(self._setting, self._offset)
+        self._volume = self.gain()
 
-    def gain(self, setting, offset):
-        if setting < 0.1:
+    def gain(self):
+        if self._setting < 0.1:
             return 0
-        db = 3.0 * (setting - offset)
+        db = 3.0 * (self._setting - self._offset)
         return pow(10.0 , db / 20.0)
--- tests/test_volume.py
+++ tests/test_volume.py
@@ -10,11 +10,17 @@ def volume_instance():
     return Volume()
 
 def test_gain(volume_instance):
-    assert volume_instance.gain(0.1, 0) == pytest.approx(1.03514216668)
-    assert volume_instance.gain(0.5, 0) == pytest.approx(1.18850222744)
-    assert volume_instance.gain(0.01, 0) == pytest.approx(0)
-    assert volume_instance.gain(9, 0) == pytest.approx(22.3872113857)
-    assert volume_instance.gain(0.01, 3) == pytest.approx(0)
+    volume_instance._setting = 0.1
+    assert volume_instance.gain() == pytest.approx(1.03514216668)
+    volume_instance._setting = 0.5
+    assert volume_instance.gain() == pytest.approx(1.18850222744)
+    volume_instance._setting = 0.01
+    assert volume_instance.gain() == pytest.approx(0)
+    volume_instance._setting = 9.0
+    assert volume_instance.gain() == pytest.approx(22.3872113857)
+    volume_instance._setting = 0.01
+    volume_instance._offset = 3.0
+    assert volume_instance.gain() == pytest.approx(0)
 
 def test_volume_initialization(volume_instance):
     assert volume_instance._setting == 9
