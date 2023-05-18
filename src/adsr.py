e72171aad2b1eb9bafc72741fb6135d9b757fa36
Nora <nob@pdx.edu>
2023-05-18 11:39:37 -0700

    save wave and envelope

--- src/adsr.py
+++ src/adsr.py
@@ -4,28 +4,30 @@ https://python.plainenglish.io/build-your-own-python-synthesizer-part-2-66396f6d
 import numpy as np
 
 class ADSREnvelope:
-    def __init__(self, attackDuration=0.05, decayDuration=0.2, sustainLevel=0.7, releaseDuration=0.3, sampleRate=48000):
+    def __init__(self, wave, attackDuration=0.05, decayDuration=0.2, sustainLevel=0.7, releaseDuration=0.3, sampleRate=48000):
         self._attack = attackDuration
         self._decay = decayDuration
         self._sustain = sustainLevel
         self._release = releaseDuration
         self._sampleRate = sampleRate
+        self._wave = wave
+        self._envelope = np.ones(len(wave)) #blank envelope
+        self.applyEnvelope()
     
-    def applyEnvelope(self, wave):
-        duration = len(wave) / self._sampleRate
+    def applyEnvelope(self):
+        #Not sure I need this variable...
+        #duration = len(self._wave) / self._sampleRate
         attackSamples = int(self._attack * self._sampleRate)
         decaySamples = int(self._decay * self._sampleRate)
         releaseSamples = int(self._release * self._sampleRate)
-        sustainSamples = len(wave) - attackSamples - decaySamples - releaseSamples
+        sustainSamples = len(self._wave) - attackSamples - decaySamples - releaseSamples
 
+        self._envelope[:attackSamples] = np.linspace(0, 1, self._attack)
+        self._envelope[attackSamples:attackSamples+decaySamples] = np.linspace(1, self._sustain, self._decay)
+        self._envelope[attackSamples+decaySamples:attackSamples+decaySamples+sustainSamples] = self._sustain
+        self._envelope[attackSamples+decaySamples+sustainSamples:] = np.linspace(self._sustain, 0, self._release)
 
-        envelope = np.ones(len(wave))
-        envelope[:attackSamples] = np.linspace(0, 1, self._attack)
-        envelope[attackSamples:attackSamples+decaySamples] = np.linspace(1, self._sustain, self._decay)
-        envelope[attackSamples+decaySamples:attackSamples+decaySamples+sustainSamples] = self._sustain
-        envelope[attackSamples+decaySamples+sustainSamples:] = np.linspace(self._sustain, 0, self._release)
-
-        return wave * envelope
+        return self._wave * self._envelope
 
 
         
