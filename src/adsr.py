d03b3ae4b02f2fbda49968dd0b0869a5206853a1
jaygermeister <37167426+jaygermeister@users.noreply.github.com>
2023-05-21 13:01:18 -0700

    test sync 1

--- src/adsr.py
+++ src/adsr.py
@@ -1,33 +1,111 @@
-'''Design inspired by 
-https://python.plainenglish.io/build-your-own-python-synthesizer-part-2-66396f6dad81'''
-
 import numpy as np
-class ADSREnvelope:
-    def __init__(self, wave, attackDuration=0.05, decayDuration=0.2, sustainLevel=0.7, releaseDuration=0.3, sampleRate=48000):
-        self._attack = attackDuration
-        self._decay = decayDuration
-        self._sustain = sustainLevel
-        self._release = releaseDuration
-        self._sampleRate = sampleRate
-        self._originalWave = wave
-        self._envelope = np.ones_like(self._originalWave) #blank envelope
-        self._createEnvelope()
-        self._modulatedWave = self.applyEnvelope()
-    
-    def _createEnvelope(self):
-        #Not sure I need this variable...
-        #duration = len(self._originalWave) / self._sampleRate
-        attackSamples = int(self._attack * self._sampleRate)
-        decaySamples = int(self._decay * self._sampleRate)
-        releaseSamples = int(self._release * self._sampleRate)
-        sustainSamples = len(self._originalWave) - attackSamples - decaySamples - releaseSamples
+import sounddevice as sd
+import threading
+import keyboard
 
-        self._envelope[:attackSamples] = np.linspace(0, 1, attackSamples)
-        self._envelope[attackSamples:attackSamples+decaySamples] = np.linspace(1, self._sustain, decaySamples)
-        self._envelope[attackSamples+decaySamples:attackSamples+decaySamples+sustainSamples] = self._sustain
-        self._envelope[attackSamples+decaySamples+sustainSamples:] = np.linspace(self._sustain, 0, releaseSamples)
-    
-    def applyEnvelope(self):
-        normalized_wave = self._originalWave / np.max(np.abs(self._originalWave)) 
-        return normalized_wave * self._envelope
-    
+# Global default variables for ADSR parameters
+attack_time = 1.0
+decay_time = 0.6
+sustain_level = 0.5
+release_time = 1.0
+key_pressed = False  # Flag to track if a key has been pressed
+
+def apply_adsr(signal, sample_rate):
+    num_samples = len(signal)
+    time = np.linspace(0, num_samples/sample_rate, num_samples)
+    envelope = np.ones_like(signal)
+
+    if attack_time > 0.0:
+        attack_samples = int(attack_time * sample_rate)
+        envelope[:attack_samples] = np.linspace(0, 1, attack_samples)[:num_samples]
+
+    if decay_time > 0.0:
+        decay_samples = int(decay_time * sample_rate)
+        if attack_time > 0.0:
+            decay_end = min(attack_samples + decay_samples, num_samples)
+            envelope[attack_samples:decay_end] = np.linspace(1, sustain_level, decay_end - attack_samples)
+        else:
+            envelope[:decay_samples] = np.linspace(1, sustain_level, decay_samples)[:num_samples]
+
+    release_samples = int(release_time * sample_rate)
+    envelope[-release_samples:] = np.linspace(sustain_level, 0, release_samples)[-num_samples:]
+
+    return signal * envelope
+
+""" debug tone for standalone ADSR development 
+def play_tone():
+    duration = 2.0  # Duration of each tone in seconds
+    sample_rate = 44100  # Sample rate in Hz
+
+    while True:
+        # Generate a simple sine wave signal
+        frequency = 440  # Frequency of the sine wave in Hz
+        time = np.linspace(0, duration, int(sample_rate * duration))
+        signal = np.sin(2 * np.pi * frequency * time)
+
+        # Apply ADSR envelope effect
+        output_signal = apply_adsr(signal, sample_rate)
+
+        # Play the signal with the ADSR envelope applied
+        sd.play(output_signal, sample_rate)
+
+        # Wait for the playback to finish
+        sd.wait()
+"""
+
+def dynamic_parameter_updates():
+    global attack_time, decay_time, sustain_level, release_time, key_pressed
+    while True:
+        if keyboard.is_pressed('a'):
+            if not key_pressed:
+                attack_time += 0.1
+                attack_time = min(attack_time, 10.0)  # Limit to a maximum value of 10.0
+                print(f"Attack time: {attack_time} (Valid range: 0.0 - 10.0)")
+                key_pressed = True
+        elif keyboard.is_pressed('s'):
+            if not key_pressed:
+                attack_time = max(attack_time - 0.1, 0.0)
+                print(f"Attack time: {attack_time} (Valid range: 0.0 - 10.0)")
+                key_pressed = True
+        elif keyboard.is_pressed('d'):
+            if not key_pressed:
+                decay_time += 0.1
+                decay_time = min(decay_time, 10.0)  # Limit to a maximum value of 10.0
+                print(f"Decay time: {decay_time} (Valid range: 0.0 - 10.0)")
+                key_pressed = True
+        elif keyboard.is_pressed('f'):
+            if not key_pressed:
+                decay_time = max(decay_time - 0.1, 0.0)
+                print(f"Decay time: {decay_time} (Valid range: 0.0 - 10.0)")
+                key_pressed = True
+        elif keyboard.is_pressed('g'):
+            if not key_pressed:
+                sustain_level += 0.1
+                sustain_level = min(sustain_level, 1.0)  # Limit to a maximum value of 1.0
+                print(f"Sustain level: {sustain_level} (Valid range: 0.0 - 1.0)")
+                key_pressed = True
+        elif keyboard.is_pressed('h'):
+            if not key_pressed:
+                sustain_level = max(sustain_level - 0.1, 0.0)
+                print(f"Sustain level: {sustain_level} (Valid range: 0.0 - 1.0)")
+                key_pressed = True
+        elif keyboard.is_pressed('j'):
+            if not key_pressed:
+                release_time += 0.1
+                release_time = min(release_time, 10.0)  # Limit to a maximum value of 10.0
+                print(f"Release time: {release_time} (Valid range: 0.0 - 10.0)")
+                key_pressed = True
+        elif keyboard.is_pressed('k'):
+            if not key_pressed:
+                release_time = max(release_time - 0.1, 0.0)
+                print(f"Release time: {release_time} (Valid range: 0.0 - 10.0)")
+                key_pressed = True
+        else:
+            key_pressed = False  # Reset the flag if no key is pressed
+
+# Create separate threads for playing the tone and updating parameters
+# tone_thread = threading.Thread(target=play_tone)
+param_thread = threading.Thread(target=dynamic_parameter_updates)
+
+tone_thread.start()
+param_thread.start()
