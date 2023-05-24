c6c17a0bacc9b2f4242914f30b39c110b9bf935a
Nora <nob@pdx.edu>
2023-05-23 20:23:13 -0700

    updated variables to snake_case

--- src/oscillator.py
+++ src/oscillator.py
@@ -13,32 +13,32 @@ class Oscillator(ABC):
     def __init__(
         self,
         frequency=220.0,
-        sampleRate=48000,
+        sample_rate=48000,
         amplitude=np.iinfo(np.int16).max / 4,
         duration=1.0,
     ):
         p = pyaudio.PyAudio()
         self._frequency: float = frequency
-        self._sampleRate: int = sampleRate
+        self._sample_rate: int = sample_rate
         self._amplitude: float = amplitude
         self._duration: float = duration
-        self._stepSize: float = 2.0 * np.pi * self._frequency / sampleRate
-        self._time = np.arange(int(self._sampleRate * self._duration))
+        self._step_size: float = 2.0 * np.pi * self._frequency / sample_rate
+        self._time = np.arange(int(self._sample_rate * self._duration))
         self._samples = np.array(0)
-        self._gainedSamples = np.array(0)
+        self._gained_samples = np.array(0)
 
-    def generateWave(self):
+    def generate_wave(self):
         pass
 
     def play(self):
-        sd.play(self._gainedSamples.astype(np.int16), self._sampleRate)
+        sd.play(self._gained_samples.astype(np.int16), self._sample_rate)
         print(self._samples[-1])
 
     # Crop the samples to the final zero crossing, so that the end of a wave match up with the beginning
     # Side effects: final signal is very slightly shorter.
     def crop_samples(self, samples):
-        samplesPerPeriod = self._sampleRate / self._frequency
-        remainder = round(self._time.size % samplesPerPeriod)
+        samples_per_period = self._sample_rate / self._frequency
+        remainder = round(self._time.size % samples_per_period)
 
         return samples[0 : self._time.size - remainder]
 
@@ -51,21 +51,21 @@ class SineOscillator(Oscillator):
     def __init__(
         self,
         frequency=440,
-        sampleRate=48000,
+        sample_rate=48000,
         amplitude=np.iinfo(np.int16).max / 4,
         duration=1.0,
     ):
         super().__init__(
             frequency=frequency,
-            sampleRate=sampleRate,
+            sample_rate=sample_rate,
             amplitude=amplitude,
             duration=duration,
         )
-        self._samples = self.generateWave()
-        self._gainedSamples = self._samples
+        self._samples = self.generate_wave()
+        self._gained_samples = self._samples
 
-    def generateWave(self):
-        samples = self._amplitude * np.sin(self._stepSize * self._time)
+    def generate_wave(self):
+        samples = self._amplitude * np.sin(self._step_size * self._time)
 
         return self.crop_samples(samples)
 
@@ -74,21 +74,21 @@ class SquareOscillator(SineOscillator):
     def __init__(
         self,
         frequency=440,
-        sampleRate=48000,
+        sample_rate=48000,
         amplitude=np.iinfo(np.int16).max / 4,
         duration=1.0,
     ):
         super().__init__(
             frequency=frequency,
-            sampleRate=sampleRate,
+            sample_rate=sample_rate,
             amplitude=amplitude,
             duration=duration,
         )
-        self._samples = self.generateWave()
-        self._gainedSamples = self._samples
+        self._samples = self.generate_wave()
+        self._gained_samples = self._samples
 
-    def generateWave(self):
-        samples = np.sin(self._stepSize * self._time)
+    def generate_wave(self):
+        samples = np.sin(self._step_size * self._time)
         for x in self._time:
             if samples[x] >= 0:
                 samples[x] = self._amplitude
@@ -104,21 +104,21 @@ class TriangleOscillator(Oscillator):
     def __init__(
         self,
         frequency=440,
-        sampleRate=48000,
+        sample_rate=48000,
         amplitude=np.iinfo(np.int16).max / 4,
         duration=1.0,
     ):
         super().__init__(
             frequency=frequency,
-            sampleRate=sampleRate,
+            sample_rate=sample_rate,
             amplitude=amplitude,
             duration=duration,
         )
-        self._samples = self.generateWave()
-        self._gainedSamples = self._samples
+        self._samples = self.generate_wave()
+        self._gained_samples = self._samples
 
-    def generateWave(self):
-        samples = np.empty(int(self._sampleRate * self._duration), dtype=float)
+    def generate_wave(self):
+        samples = np.empty(int(self._sample_rate * self._duration), dtype=float)
 
         # half-period
         hp = (1 / self._frequency) / 2
@@ -130,7 +130,7 @@ class TriangleOscillator(Oscillator):
             samples[x] = (
                 da
                 / hp
-                * (hp - np.abs(np.mod(x / self._sampleRate + hp / 2, (2 * hp)) - hp))
+                * (hp - np.abs(np.mod(x / self._sample_rate + hp / 2, (2 * hp)) - hp))
                 - self._amplitude
             )
 
@@ -142,27 +142,27 @@ class SawtoothOscillator(Oscillator):
     def __init__(
         self,
         frequency=440,
-        sampleRate=48000,
+        sample_rate=48000,
         amplitude=np.iinfo(np.int16).max / 4,
         duration=1.0,
     ):
         super().__init__(
             frequency=frequency,
-            sampleRate=sampleRate,
+            sample_rate=sample_rate,
             amplitude=amplitude,
             duration=duration,
         )
-        self._samples = self.generateWave()
-        self._gainedSamples = self._samples
+        self._samples = self.generate_wave()
+        self._gained_samples = self._samples
 
-    def generateWave(self):
-        samples = np.arange(self._sampleRate * self._duration)
+    def generate_wave(self):
+        samples = np.arange(self._sample_rate * self._duration)
 
         for x in self._time:
             samples[x] = (
                 2
                 * np.fmod(
-                    ((x * self._frequency * self._amplitude) / self._sampleRate)
+                    ((x * self._frequency * self._amplitude) / self._sample_rate)
                     + self._amplitude / 2,
                     self._amplitude,
                 )
