da11456c3e701cc5e60bd351145135de657adaf0
Rohan Singh <vicky94rohan@gmail.com>
2023-05-23 23:42:22 -0700

    Main window dimensions updated to accommodate latest UI changes

--- src/main.py
+++ src/main.py
@@ -5,13 +5,13 @@ from form import MainWidget
 from PySide6.QtWidgets import QApplication
 from PySide6.QtCore import QSize
 
-WINDOW_WIDTH = 610
-WINDOW_HEIGHT = 313
-
+WINDOW_WIDTH = 776
+WINDOW_HEIGHT = 444
 
 if __name__ == "__main__":
     app = QApplication([])
     widget = MainWidget()
+    widget.setWindowTitle("Snake Synth")
     widget.setFixedSize(QSize(WINDOW_WIDTH, WINDOW_HEIGHT))
     widget.show()
     sys.exit(app.exec())
