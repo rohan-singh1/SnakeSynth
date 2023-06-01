58238f3834777473fa9b9e33a4f11a3c549158cf
Nora <nob@pdx.edu>
2023-06-01 16:07:13 -0700

    main dev docs updated

--- src/main.py
+++ src/main.py
@@ -1,4 +1,15 @@
 # This Python file uses the following encoding: utf-8
+'''
+Code Structure
+The Snake Synth code consists of two main files: form.py and main.py. The form.py file contains the 
+implementation of the GUI using PySide6 widgets, while main.py acts as the entry point for the application.
+
+To use the synthesizer, follow these steps:
+
+1. Make sure you have all the required dependencies installed, including PySide6, NumPy, and sounddevice.
+You can use the command 'pip install -r requirements.txt' in the root directory.
+2. To run the synthesizer, use the command 'python3 main.py'
+'''
 
 import sys
 from form import MainWidget
