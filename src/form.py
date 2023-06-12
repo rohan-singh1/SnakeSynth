d652f91d2d9adcb4af93370ba903602ea6d9a659
jaygermeister <37167426+jaygermeister@users.noreply.github.com>
2023-06-11 18:15:49 -0700

    MIDI input enabled with error handling

--- src/form.py
+++ src/form.py
@@ -100,93 +100,16 @@ selected = sine_waves
 
 # Key names in the GUI
 GUI_KEY_NAMES = [
-            "C0",
-            "C#0",
-            "D0",
-            "D#0",
-            "E0",
-            "F0",
-            "F#0",
-            "G0",
-            "G#0",
-            "A0",
-            "A#0",
-            "B0",
-            "C1",
-            "C#1",
-            "D1",
-            "D#1",
-            "E1",
-            "F1",
-            "F#1",
-            "G1",
-            "G#1",
-            "A1",
-            "A#1",
-            "B1",
-            "C2",
-            "C#2",
-            "D2",
-            "D#2",
-            "E2",
-            "F2",
-            "F#2",
-            "G2",
-            "G#2",
-            "A2",
-            "A#2",
-            "B2",
-            "C3",
-            "C#3",
-            "D3",
-            "D#3",
-            "E3",
-            "F3",
-            "F#3",
-            "G3",
-            "G#3",
-            "A3",
-            "A#3",
-            "B3",
-            "C4",
-            "C#4",
-            "D4",
-            "D#4",
-            "E4",
-            "F4",
-            "F#4",
-            "G4",
-            "G#4",
-            "A4",
-            "A#4",
-            "B4",
-            "C5",
-            "D5",
-            "D#5",
-            "E5",
-            "F5",
-            "F#5",
-            "G5",
-            "G#5",
-            "A5",
-            "A#5",
-            "B5",
-            "C6",
-            "D6",
-            "D#6",
-            "E6",
-            "F6",
-            "F#6",
-            "G6",
-            "G#6",
-            "A6",
-            "A#6",
-            "B6",
-            "C7", 
-            "D7",
-            "D#7",
-            "E7"
-        ]
+    "C0", "C#0", "D0", "D#0", "E0", "F0", "F#0", "G0", "G#0", "A0", "A#0", "B0",
+    "C1", "C#1", "D1", "D#1", "E1", "F1", "F#1", "G1", "G#1", "A1", "A#1", "B1",
+    "C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2",
+    "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3",
+    "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4",
+    "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5",
+    "C6", "C36", "D6", "D#6", "E6", "F6", "F#6", "G6", "G#6", "A6", "A#6", "B6",
+    "C7", "C#7", "D7" ,"D#7", "E7", "F7", "F#7", "G7", "G#7", "A7", "A#7", "B7"
+]
+
 
 # Thread worker
 class Worker(QRunnable):
@@ -382,13 +305,20 @@ class MainWidget(
     # Define a method for handling button releases
     def button_pressed_handler(self, key):
         key_mapping = list(zip(GUI_KEY_NAMES, self.pitch_shifted_keys))
+        mapped_key = None  # Initialize mapped_key with a default value for MIDI input handling
+
         for pair in key_mapping:
             if key == pair[0]:
                 mapped_key = pair[1]
+                break  # exit loop once match is found
 
-        self.adsr_envelope.update_state(State.ATTACK)
-        worker = Worker(self.play_loop, gained_waves[mapped_key])
-        self.threadpool.start(worker)
+        if mapped_key is not None:  # Check if a valid mapped_key value was found
+            self.adsr_envelope.update_state(State.ATTACK)
+            worker = Worker(self.play_loop, gained_waves[mapped_key])
+            self.threadpool.start(worker)
+        else:
+            # ignore and continue in NONE case  
+            pass 
 
     def button_released_handler(self):
         self.adsr_envelope.update_state(State.RELEASE)
